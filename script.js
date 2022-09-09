76ec9e48b69483af6ba5014b3cc5ef08ffcf3ec5
Jon Hause <jonrhause@gmail.com>
2022-09-09 16:28:53 -0600

    Experiment with HTML, CSS, & JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const h2 = document.createElement('h2');
+
 h2.textContent = "I'm the goddamn Batman";
+
 document.querySelector('body').appendChild(h2);
+
+const input = document.getElementsByClassName("Terminator")[0];
+
+function clickAlert () {
+    alert('Hasta la vista, bay-bee!');
+};
+
+input.addEventListener('click', clickAlert);
